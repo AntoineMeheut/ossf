f0a999ed9c8e46e994411ae222fe69eebc4acde7
Antoine.Meheut <34522927+AntoineMeheut@users.noreply.github.com>
2025-05-24 10:04:06 +0200

    Update nexus-ubuntu-server-20.04.sh

--- install-nexus/nexus-ubuntu-server-20.04.sh
+++ install-nexus/nexus-ubuntu-server-20.04.sh
@@ -6,8 +6,7 @@
 #
 # INFORMATIONS
 # Distribution        : Ubuntu 20.04 64-Bit
-# Nexus Version       : TODO
-# PostgreSQL Version  : latest version
+# Nexus Version       : latest
 #
 # USAGE
 #   wget -O ~/nexus-ubuntu-server-20.04.sh https://raw.githubusercontent.com/AntoineMeheut/ossf/refs/heads/main/install-nexus/nexus-ubuntu-server-20.04.sh
@@ -61,67 +60,84 @@ run_command log "Install OpenJDK 8..."
 run_command sudo apt install -y openjdk-8-jre-headless
 
 ##
-# Install net-tools
-#
-run_command sudo apt install -y net-tools
-
 # Navigate to the /opt directory
+#
+run_command log "Navigate to the /opt directory..."
 run_command cd /opt
 
+##
 # Download Nexus
-sudo wget "$download_url"
+#
+run_command log "Download Nexus..."
+run_command sudo wget "$download_url"
 
+##
 # Extract Nexus version from the downloaded file name
+#
+run_command log "Extract Nexus version from the downloaded file name..."
 run_command nexus_version=$(tar tzf "$downloaded_file" | grep -oP 'nexus-3\.\d+\.\d+-\d+' | head -n 1)
-
-# Extract Nexus
 run_command sudo tar -zxvf "$downloaded_file"
 
+##
 # Create a Nexus user (modify as needed for password input)
+#
+run_command log "Create a Nexus user (modify as needed for password input)..."
 run_command sudo useradd -m -s /bin/bash -U nexus
 
-# Set password for the Nexus user password: nexus123
-#Change with your own password
-echo "nexus:nexus123" | sudo chpasswd
+##
+# Change password for the Nexus user password: nexus123
+#
+run_command log "Change password for the Nexus user password: nexus123..."
+run_command echo "nexus:nexus123" | sudo chpasswd
 
+##
 # Set ownership for Nexus directories (checking if nexus_version is set)
+#
+run_command log "Set ownership for Nexus directories (checking if nexus_version is set)..."
 if [ -n "$nexus_version" ]; then
-  sudo chown -R nexus:nexus "$nexus_version"
-  sudo chown -R nexus:nexus sonatype-work
+  run_command sudo chown -R nexus:nexus "$nexus_version"
+  run_command sudo chown -R nexus:nexus sonatype-work
 else
-  echo "Failed to determine Nexus version. Check the script and try again."
+  run_command log "Failed to determine Nexus version. Check the script and try again."
   exit 1
 fi
 
+##
 # Edit Nexus runtime configuration (checking if nexus_version is set)
+#
+run_command log "Edit Nexus runtime configuration (checking if nexus_version is set)..."
 if [ -n "$nexus_version" ]; then
-  sudo sed -i 's/^run_as_user=.*/run_as_user="nexus"/' "/opt/$nexus_version/bin/nexus.rc"
+  run_command sudo sed -i 's/^run_as_user=.*/run_as_user="nexus"/' "/opt/$nexus_version/bin/nexus.rc"
 else
-  echo "Failed to determine Nexus version. Check the script and try again."
+  run_command log "Failed to determine Nexus version. Check the script and try again."
   exit 1
 fi
 
+##
 # Switch to the Nexus user and start Nexus
-sudo su - nexus <<EOF
+#
+run_command log "Switch to the Nexus user and start Nexus..."
+run_command sudo su - nexus <<EOF
 /opt/$nexus_version/bin/nexus start
 EOF
 
+##
 # Wait for 60 seconds to allow Nexus to initialize
-echo "Waiting for Nexus to initialize..."
-sleep 60
+#
+run_command log "Wait for 60 seconds to allow Nexus to initialize..."
+run_command sleep 60
 
+##
 # Check Nexus process status
-ps aux | grep nexus
+#
+run_command log "Check Nexus process status..."
+run_command ps aux | grep nexus
 
+##
 # Check Nexus port status
-netstat -lnpt
-
-# Print admin password
-echo -e "=========== Admin Password ===========\n"
-cat /opt/sonatype-work/nexus3/admin.password
-echo -e "\n"
-echo -e "=========== Admin Password ==========="
-
+#
+run_command log "Check Nexus port status..."
+run_command netstat -lnpt
 
 ##
 # Final message
@@ -129,8 +145,8 @@ echo -e "=========== Admin Password ==========="
 run_command log "*==================================================================*"
 run_command log "Nexus successfully installed. Access it via http://your_server_ip:8080."
 run_command log "Your admin password is:"
-run_command docker compose logs initializer | grep "Admin password:"
-run_command log " "
+run_command cat /opt/sonatype-work/nexus3/admin.password
+run_command log "\n"
 run_command log "Script written by Antoine Meheut, 2025."
 run_command log "https://github.com/AntoineMeheut"
 run_command log "*==================================================================*"
